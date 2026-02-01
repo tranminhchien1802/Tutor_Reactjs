5e7d872f3549576f1ca399549b833db1d2fd11df
Tran Minh Chien <chien180203@gmail.com>
2026-02-01 14:59:02 +0700

    Cập nhật URL Google Maps để tăng khả năng tương thích với môi trường deploy

--- src/Component/map.js
+++ src/Component/map.js
@@ -44,7 +44,7 @@ const MapComponent = () => {
         </a>
       </div>
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.682388598594!2d106.7727213748491!3d10.85135098929837!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3175273d52dd4b5f%3A0x5e0a1a0a1a0a1a0a!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBUaMOgbmcgdHLDtCBL4bu5IFRo4buRdCBUaMOgbmgIHBoxrDhu51jIEjGsMahbmc!5e0!3m2!1svi!2s"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.682388598594!2d106.7727213748491!3d10.85135098929837!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3175273d52dd4b5f%3A0x5e0a1a0a1a0a1a0a!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBUaMOgbmcgdHLDtCBL4bu5IFRo4buRdCBUaMOgbmgIHBoxrDhu51jIEjGsMahbmc!5e0!3m2!1sen!2s"
         width="100%"
         height="100%"
         style={{ border: 0 }}
